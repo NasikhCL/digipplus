01ba1cf272c00110616f1f38856d832890485ce1
Nasikh CL <nasikcl@gmail.com>
2022-11-19 13:47:44 +0530

    react router added, link added to the nav for all 3 pages

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MyJobs from "./pages/MyJobs";
 import MyPayment from "./pages/MyPayments";
-
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
-      {/* <MyJobs /> */}
-      <MyPayment />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/my-jobs" element={<MyJobs />} />
+        <Route exact path="/my-payment" element={<MyPayment />} />
+      </Routes>
+    
     </div>
   );
 }
 
 export default App;
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import './css/navbar.css'
-
+import { Link } from "react-router-dom";
 export default function Navbar(){
+
     return(
         <div className="navbar">
             <nav>
                 <img className="logo-img" src="https://digipplus.com/static/homepage/imgs/digipplus-logo-70x70.38a25d5cef0d.png" alt="logo"/>
                 <h2 className="app-title">Digipplus</h2>
                 <ul>
-                    <li>HOME</li>
-                    <li>My Jobs</li>
-                    <li>My Payments</li>
+                  <Link to="/"><li>HOME</li></Link>  
+                  <Link to="/my-jobs"><li>My Jobs</li></Link>  
+                  <Link to="/my-payment"><li>My Payments</li></Link>    
                 </ul>
                 <img className="user-profile" src="https://images.pexels.com/photos/14391922/pexels-photo-14391922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="user-profile"/>
             </nav>
